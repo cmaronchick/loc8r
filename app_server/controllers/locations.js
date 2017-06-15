fbdcc3d7331456e4d18645aeb20340def9567929
Chris Aronchick <chrisaronchick@gmail.com>
2017-06-14 21:46:59 -0700

    First set of jade updates

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,15 @@
+/* GET 'home' page */
+module.exports.homeList = function(req, res) {
+    res.render('index', {title: 'Home'});
+};
+
+/* GET 'Location Info' Page */
+module.exports.locationInfo = function (req, res) {
+    res.render('index', {title: 'Location Info'});
+};
+
+/* GET 'Add review' Page */
+
+module.exports.addReview = function (req, res) {
+    res.render('index', {title: 'Add review'});
+};
