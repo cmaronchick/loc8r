66e16fa12ed348591182282d5fb1b7e10aaa8460
Chris Aronchick <chrisaronchick@gmail.com>
2017-06-29 22:36:45 -0700

    Making progress on chapter 10, but having issues with the body ng-view

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ var appClientFiles = [
   "app_client/common/services/geolocation.service.js",
   "app_client/common/services/loc8rData.service.js",
   "app_client/common/filters/formatDistance.filter.js",
-  "app_client/common/directives/ratingStars.directive.js"
+  "app_client/common/directives/ratingStars/ratingStars.directive.js",
+  "app_client/common/directives/footerGeneric/footerGeneric.directive.js",
+  "app_client/common/directives/navigationGeneric/navigationGeneric.directive.js",
+  "app_client/common/directives/pageHeader/pageHeader.directive.js"
 ];
 
 var uglified = uglifyJs.minify(appClientFiles, { compress : false });
---
+++ app_client/common/directives/navigationGeneric/navigationGeneric.directive.js
@@ -0,0 +1,11 @@
+(function() {
+    angular
+        .module('loc8rApp')
+        .directive('navigationGeneric', navigationGeneric);
+    function navigationGeneric () {
+        return {
+            restrict: 'EA',
+            templateUrl: '/common/directives/navigationGeneric.template.html'
+        };
+    }
+})();
